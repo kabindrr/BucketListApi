72258309fa2ed18fcfb933c3781db04bfe612935
kabindrr <chaina_kabindra@yahoo.com>
2024-10-16 22:18:34 +1100

    serving static server

--- server.js
+++ server.js
@@ -7,14 +7,26 @@ import { bucketRouter } from "./routes/bucketRouter.js";
 const app = express();
 const PORT = 3001;
 
+//database
+mongooseDbConnection();
+
 //middlewares
 app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
 app.use("/api/v1/bucketList", bucketRouter);
 
-//database
-mongooseDbConnection();
+//static serving
+import path from "path";
+const _dirname = path.resolve();
+
+//serve the static files from the node
+app.use(express.static(path.join(_dirname, "dist")));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(_dirname, "dist", "index.html"));
+});
+
+console.log(_dirname);
 
 app.listen(PORT, (error) => {
   error
